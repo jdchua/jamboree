b90ffcd58250f540f3398be00f93309140f88987
Jeremiah Chua <jeremiahdanielchua@gmail.com>
2019-01-04 05:25:42 +0000

    change console log for app file

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("YelpCamp server is running bizzznitchhh!!!"); 
+   console.log("FestivalCamp Has Started!!!"); 
 });
